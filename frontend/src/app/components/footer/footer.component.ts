68991ad0d0cfc430183b500172114dc2949e0077
Meriem IKHARRAZEN <meriem.ikharrazen@gmail.com>
2021-06-03 20:50:18 +0100

    add home page

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -14,3 +14,30 @@ export class FooterComponent implements OnInit {
   }
 
 }
+
+// Preloader
+$(window).on('load', function() {
+  if ($('#preloader').length) {
+    $('#preloader').delay(100).fadeOut('slow', function() {
+      $(this).remove();
+    });
+  }
+});
+
+  // Back to top button
+  $(window).on('scroll',function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $(document).on('click','.back-to-top',function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+
