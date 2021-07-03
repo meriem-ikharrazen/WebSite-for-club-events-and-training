2a9fe56fe165534d2d4897daef0390c04ccd5968
Meriem IKHARRAZEN <meriem.ikharrazen@gmail.com>
2021-07-03 21:07:45 +0100

    changes in home page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Evenement } from 'app/models/evenement.model';
+import { Formateur } from 'app/models/formateur.model';
+import { EvenementService } from 'app/services/evenement.service';
+import { FormateurService } from 'app/services/formateur.service';
+import { NotificationService } from 'app/services/notification.service';
+import { GlobalVariables } from 'GlobalVariables';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public formateurs: Formateur[] = [];
+  public events: Evenement[] = [];
+  public allFormateurs: Formateur[] = [];
+  public pageSlice:Formateur[] = [];
+  public showFormateur : Formateur = new Formateur();
+  public searchTxt:string = '';
+  public filterValue:string = ';'
+  public startIndex = 0;
+  public endIndex = 4;
+  public path = '';
+  public chemin='http://localhost:8080/public/images/evenements/';
+  constructor(private eventService:EvenementService,private formateurService: FormateurService,private notificationService: NotificationService,private global:GlobalVariables){}
 
   ngOnInit(): void {
+    this.path = this.global.url +'/';
+    this.getAllFormateurs();
+    this.getAllEvents();
   }
 
+  public getAllEvents() {
+    this.eventService.getEvenements().subscribe((result: Evenement[]) => {
+      this.events = result.slice(this.startIndex,this.endIndex);
+    });
+  } 
+
+  public getAllFormateurs() {
+    this.formateurService.getFormateurs().subscribe((result: Formateur[]) => {
+      this.formateurs =result.slice(this.startIndex,this.endIndex);
+    });
+  }
 }
